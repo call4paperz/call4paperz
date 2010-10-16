43bebe2dc7858e9e77ce10ac21a5ebc1531d9898
Douglas Campos <douglas@theros.info>
2010-10-16 13:07:07 -0300

    twitter/facebook login icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
       if user_signed_in?
         content << content_tag(:li, link_to("Logout", destroy_user_session_path))
       else
-        content << content_tag(:li, link_to("Login Twitter", '/auth/twitter'))
-        content << content_tag(:li, link_to("Login Facebook", '/auth/facebook'))
+        content << content_tag(:li, link_to("Login Twitter", '/auth/twitter', :class => "auth_icon twitter_auth"))
+        content << content_tag(:li, link_to("Login Facebook", '/auth/facebook', :class => "auth_icon facebook_auth"))
       end
       content << content_tag(:li, link_to("Events", events_path))
       content << content_tag(:li, link_to("Home", root_url))

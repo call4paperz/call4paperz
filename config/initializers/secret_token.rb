e0d9555e6f4de23a42b2ff813de7c780b588a58a
Vinicius Baggio <vinibaggio@gmail.com>
2010-10-15 21:01:22 -0300

    F1RSTga .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rr10Team71::Application.config.secret_token = '55181ae351020628bfe4eb0850c55e63f514f0721a7b16ad9b7c1cee1c0b8250132e29b66110dcc1c7c22418274a05be7ff3356ae0570dd03fac2d61321fd461'

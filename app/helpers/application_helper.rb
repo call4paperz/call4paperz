c82c1a5abb34967d7e58180dc7df6783e99eebf7
Vinicius Baggio <vinibaggio@gmail.com>
2010-10-16 05:09:00 -0300

    Removing warnings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,17 @@
 module ApplicationHelper
   def menu
-    content_tag :ul, :id  => "menu_top" do 
+    content_tag :ul, :id  => "menu_top" do
       content = ''
-
       if user_signed_in?
-        content << content_tag (:li, link_to ("Logout", destroy_user_session_path))        
+        content << content_tag(:li, link_to("Logout", destroy_user_session_path))
       else
-        content << content_tag (:li, link_to ("Login Twitter", '/auth/twitter'))
-        content << content_tag (:li, link_to ("Login Facebook", '/auth/facebook'))
-      end    
-
-      content << content_tag (:li, link_to ("Events", events_path))
-      content << content_tag (:li, link_to ("Home", root_url))
-
+        content << content_tag(:li, link_to("Login Twitter", '/auth/twitter'))
+        content << content_tag(:li, link_to("Login Facebook", '/auth/facebook'))
+      end
+      content << content_tag(:li, link_to("Events", events_path))
+      content << content_tag(:li, link_to("Home", root_url))
       content
-    end    
+    end
   end
 end
 

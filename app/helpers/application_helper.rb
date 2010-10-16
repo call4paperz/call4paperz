7cefcde3f1460f6703dc085c737de6af760da7e0
Vinicius Baggio <vinibaggio@gmail.com>
2010-10-16 14:46:43 -0300

    Fixing menu on IE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,14 @@ module ApplicationHelper
   def menu
     content_tag :ul, :id  => "menu_top" do
       content = ''
+      content << content_tag(:li, link_to("Home", root_url))
+      content << content_tag(:li, link_to("Events", events_path))
       if user_signed_in?
         content << content_tag(:li, link_to("Logout", destroy_user_session_path))
       else
         content << content_tag(:li, link_to("Login Twitter", '/auth/twitter', :class => "auth_icon twitter_auth"))
         content << content_tag(:li, link_to("Login Facebook", '/auth/facebook', :class => "auth_icon facebook_auth"))
       end
-      content << content_tag(:li, link_to("Events", events_path))
-      content << content_tag(:li, link_to("Home", root_url))
       content
     end
   end

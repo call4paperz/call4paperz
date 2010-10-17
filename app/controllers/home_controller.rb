475f6151b16c104a37f6d03c571aaac6dec7c3ea
Vinicius Baggio <vinibaggio@gmail.com>
2010-10-17 11:59:46 -0200

    Removing twitter gem and using fromt side javascript

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,6 @@ class HomeController < ApplicationController
     @events = Event.most_recent
     @comments = Comment.most_recent
     @events_quantity = Event.count
-    latest_tweet
   end
 
-  def latest_tweet
-    @latest_tweet ||= Twitter.user('call4paperz')["status"]["text"]
-  rescue StandardError
-    @latest_tweet = ''
-  end
 end

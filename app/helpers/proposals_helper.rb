578d65daa7fbdae3e26502cc3eae4b812380c4c7
Douglas Campos <douglas@theros.info>
2010-10-17 01:27:04 -0200

    message normalization

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -1,7 +1,7 @@
 module ProposalsHelper
   def vote_box(event, proposal, user)
     if user && user.has_vote_for?(proposal)
-      "You've already voted"
+      "Thanks for voting!"
     else
       content = ''
       content << link_to(image_tag('positive.png', :class => 'thumb'), like_event_proposal_path(event, proposal), {:remote => true, :class => "ajax_vote"})

489b4ec74f7e386e8cbacf9e23c07b9406530af6
Vinicius Baggio <vinibaggio@gmail.com>
2010-10-17 02:36:00 -0200

    Rescuing from tweet failures if requests are over the demand

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,12 @@ class HomeController < ApplicationController
     @events = Event.most_recent
     @comments = Comment.most_recent
     @events_quantity = Event.count
-    @latest_tweet = Twitter.user('call4paperz')["status"]["text"]
+
+  end
+
+  def latest_tweet
+    @latest_tweet ||= Twitter.user('call4paperz')["status"]["text"]
+  rescue StandardError
+    @latest_tweet = ''
   end
 end

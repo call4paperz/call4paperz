711ab7e4c28eb9154185ab676cd1c39004c1eb5a
Douglas Campos <douglas@theros.info>
2010-10-16 21:37:28 -0300

    ajaxifying links

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -4,8 +4,8 @@ module ProposalsHelper
       "You've already voted"
     else
       content = ''
-      content << link_to(image_tag('positive.png', :class => 'thumb'), like_event_proposal_path(event, proposal))
-      content << link_to(image_tag('negative.png', :class => 'thumb'), dislike_event_proposal_path(event, proposal))
+      content << link_to(image_tag('positive.png', :class => 'thumb'), like_event_proposal_path(event, proposal), {:remote => true},  :class => "ajax_vote")
+      content << link_to(image_tag('negative.png', :class => 'thumb'), dislike_event_proposal_path(event, proposal), {:remote => true}, :class => "ajax_vote")
       content.html_safe
     end
   end

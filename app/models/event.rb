a76f85bbf2dd49179b31ad6d15b8a65277e02362
Vinicius Baggio <vinibaggio@gmail.com>
2010-10-17 12:59:02 -0200

    Add twitter validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,10 +9,26 @@ class Event < ActiveRecord::Base
   validates_presence_of :name, :description, :occurs_at
   validates_associated :user
 
+  before_validation :twitter_has_valid_format
+
   mount_uploader :picture, PictureUploader
 
   def self.most_recent
     order("created_at DESC").limit(3)
+  end
+
+  private
+  def twitter_has_valid_format
+    match = twitter &&
+      (twitter.match(/^([a-zA-Z0-9_]{1,15})$/) ||
+       twitter.match(/^@([a-zA-Z0-9_]{1,15})$/) ||
+       twitter.match(/^(http:\/\/)?www\.twitter\.com\/([a-zA-Z0-9_]{1,15})$/))
+
+    if match
+      self.twitter = match[-1]
+    elsif twitter.present?
+      errors.add(:twitter, :invalid)
+    end
   end
 
 end

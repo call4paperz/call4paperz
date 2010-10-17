0153ac2f57f37f4b67c918ec6b0698186a2b39ba
Vinicius Baggio <vinibaggio@gmail.com>
2010-10-17 01:44:38 -0200

    Fixing specs for application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,3 @@ module ApplicationHelper
     end
   end
 end
-
-
-
-
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -16,10 +16,8 @@ describe ApplicationHelper do
       it { should match(events_path) }
       it { should match(/Logout/) }
       it { should match(destroy_user_session_path) }
-      it { should_not match(/Login Twitter/) }
-      it { should_not match('/auth/twitter') }
-      it { should_not match(/Login Facebook/) }
-      it { should_not match('/auth/facebook') }
+      it { should_not match(/Login/) }
+      it { should_not match(new_user_session_path) }
     end
 
     context "not logged in" do
@@ -30,10 +28,8 @@ describe ApplicationHelper do
       it { should match(root_url) }
       it { should match(/Events/) }
       it { should match(events_path) }
-      it { should match(/Login Twitter/) }
-      it { should match('/auth/twitter') }
-      it { should match(/Login Facebook/) }
-      it { should match('/auth/facebook') }
+      it { should match(/Login/) }
+      it { should match(new_user_session_path) }
       it { should_not match(/Logout/) }
       it { should_not match(destroy_user_session_path) }
     end

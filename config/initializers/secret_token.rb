9ef97f619655e9449affd9605f334b74d25118e7
Vinicius Baggio <vinibaggio@gmail.com>
2012-08-25 17:41:04 -0700

    Resetting session key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rr10Team71::Application.config.secret_token = 'f90581d6c81bd68675852ebba81d7c64288bb41a40c8ea322932a93820a74576f78a2f9ad3bee7d58eb864281a7b4fce842a522277197ff5d969ebc3568fa4b3'
+Rr10Team71::Application.config.secret_token = ENV['SECRET_KEY']

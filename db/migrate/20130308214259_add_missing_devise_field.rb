1b2a970483ed8ee3833818515186f94707dc4d2b
Vinicius Baggio <vinibaggio@gmail.com>
2013-03-13 20:48:27 -0700

    Changing missing devise field migration.
    This was created to support databases created from scratch.
    Since our prod database has been created a long time, devise
    migrations end up with different schemas, and thus this hack
    is necessary. Shouldn't affect any existing code.

--- db/migrate/20130308214259_add_missing_devise_field.rb
+++ db/migrate/20130308214259_add_missing_devise_field.rb
@@ -1,5 +1,9 @@
 class AddMissingDeviseField < ActiveRecord::Migration
+  User = Class.new(ActiveRecord::Base)
+
   def change
-    add_column :users, :reset_password_sent_at, :datetime
+    unless User.column_names.include? 'reset_password_sent_at'
+      add_column :users, :reset_password_sent_at, :datetime
+    end
   end
 end

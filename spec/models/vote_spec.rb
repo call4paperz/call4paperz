03d77bfaa7123d0cbdaaa244c1a9c339a97f0f84
Mauro George <maurogot@gmail.com>
2013-09-24 18:56:51 -0300

    Add mass assigment spec to Vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,6 +4,17 @@ describe Vote do
   let(:proposal) { FactoryGirl.create(:proposal) }
   let(:user) { FactoryGirl.create(:user) }
 
+  describe "mass assignment" do
+
+    context "not allowed" do
+      [:id, :direction, :proposal_id, :user_id, :created_at, :updated_at].each do |attr|
+        it { should_not allow_mass_assignment_of(attr) }
+      end
+    end
+  end
+
+
+
   describe ".like!" do
     it "should generate a vote for the specified proposal and user" do
       expect {

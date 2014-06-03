91d25fb89787b7095e43631555ce07d30bf5f586
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-06-03 20:14:16 -0300

    Configures a host for assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rr10Team71::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://call4paperz.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( 404.css )

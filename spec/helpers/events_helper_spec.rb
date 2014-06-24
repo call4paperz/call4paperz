def65a8f7394d58785da63632628296d9a38a3c2
Americo Duarte <americodls@gmail.com>
2014-06-23 23:22:01 -0300

    Avoiding deprecation messages about rspec

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -3,23 +3,23 @@ require 'spec_helper'
 describe EventsHelper do
   describe "#user_is_owner?" do
     it "reports true if they match" do
-      user_stub = stub
-      model_mock = mock(:user => user_stub)
+      user_stub = double
+      model_mock = double(:user => user_stub)
 
       helper.should_receive(:user_signed_in?).and_return(true)
       helper.should_receive(:current_user).and_return(user_stub)
 
-      helper.user_is_owner?(model_mock).should be_true
+      helper.user_is_owner?(model_mock).should be true
     end
 
     it "reports false if they don't" do
-      user_stub = stub
-      model_mock = mock(:user => stub)
+      user_stub = double
+      model_mock = double(:user => double)
 
       helper.should_receive(:user_signed_in?).and_return(true)
       helper.should_receive(:current_user).and_return(user_stub)
 
-      helper.user_is_owner?(model_mock).should be_false
+      helper.user_is_owner?(model_mock).should be false
     end
   end
 end

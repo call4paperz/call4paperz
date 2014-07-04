0eb9f1e63f0ee4c2fd235c1e9fddcb37208601f6
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-07-03 23:10:40 -0300

    changes some expectations to the “new” (spec 3) syntax

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -6,20 +6,20 @@ describe EventsHelper do
       user_stub = double
       model_mock = double(:user => user_stub)
 
-      helper.should_receive(:user_signed_in?).and_return(true)
-      helper.should_receive(:current_user).and_return(user_stub)
+      expect(helper).to receive(:user_signed_in?).and_return(true)
+      expect(helper).to receive(:current_user).and_return(user_stub)
 
-      helper.user_is_owner?(model_mock).should be true
+      expect(helper.user_is_owner? model_mock).to be true
     end
 
     it "reports false if they don't" do
       user_stub = double
       model_mock = double(:user => double)
 
-      helper.should_receive(:user_signed_in?).and_return(true)
-      helper.should_receive(:current_user).and_return(user_stub)
+      expect(helper).to receive(:user_signed_in?).and_return(true)
+      expect(helper).to receive(:current_user).and_return(user_stub)
 
-      helper.user_is_owner?(model_mock).should be false
+      expect(helper.user_is_owner? model_mock).to be false
     end
   end
 end

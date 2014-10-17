0058961a59be04f1ba27fac945c98769ae0fba5e
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-10-17 19:56:57 -0300

    Ask email for users that doesn’t have it already.
    For now, just authenticated users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
+  # An user that haven't provide an email, have to do it now.
+  before_filter do
+    check_for_email_presence if user_signed_in?
+  end
+
   def store_location
     return if skip_store_location
 
@@ -18,5 +23,18 @@ class ApplicationController < ActionController::Base
 
   def skip_store_location
     false
+  end
+
+  private
+  def profile_completing?
+    params['controller'] == 'profiles' &&
+       (params['action'] == 'update' || params['action'] == 'edit')
+  end
+
+  def check_for_email_presence
+    need_completion = !current_user.email.present?
+    if need_completion && !profile_completing?
+      redirect_to edit_profile_path(current_user)
+    end
   end
 end

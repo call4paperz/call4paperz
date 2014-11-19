27f289f3b006902bfb7f738f61296c4c2cd3d35d
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-11-19 19:48:58 -0200

    Refactor: :scissor: Reduce the conditionals and bumps to aggregate authentications for signed_in users.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,47 +5,36 @@ class AuthenticationsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    if authenticate!(auth['provider'], auth['uid'], auth['user_info'])
-      destination_url = stored_location_for(:user) || root_path
-      redirect_to destination_url, :notice => "Welcome #{current_user.name}!"
+    destination_url = stored_location_for(:user) || root_path
+
+    if user_signed_in?
+      current_user.authentications.create(provider: auth['provider'], uid: auth['uid'])
+      will_redirect = true
+    else
+      # As soon as we update the rails version, this will redirect will be
+      # sacrificed to the gods of the good code.
+      # It is here now because the redirect_to in our version doesn't do the
+      # shortcircuit. And the authenticate! method, can call redirections.
+      will_redirect = authenticate!(auth['provider'], auth['uid'], auth['user_info'])
     end
+
+    redirect_to destination_url, notice: "Welcome #{current_user.name}!" if will_redirect
   end
 
   def failure
-    redirect_to root_url, :notice => "Authentication error: #{params[:message].humanize}"
+    redirect_to root_url, notice: "Authentication error: #{params[:message].humanize}"
   end
 
   private
-  def sign_in_with_authentication(authentication)
-    if user_signed_in?
-      current_user.authentications.create(provider: authentication.provider, uid: uid)
-    else
-      sign_in authentication.user
-    end
-    true
-  end
-
-  def sign_in_with_twitter(uid)
-    if user_signed_in?
-      current_user.authentications.create(provider: 'twitter', uid: uid)
-      true
-    else
-      redirect_to new_user_session_path, notice: I18n.t('auth.cant_create_twitter')
-      false
-    end
-  end
 
   def create_authentication_and_sign_in(provider, uid, auth_info)
-    user = User.create_from_auth_info(provider, uid, auth_info)
-    sign_in(user)
-    true
-  end
-
-  def create_authentication(provider, uid, auth_info)
     if provider.to_s == 'twitter'
-      sign_in_with_twitter(uid)
+      redirect_to new_user_session_path, notice: I18n.t('auth.cant_create_twitter')
+      false
     else
-      create_authentication_and_sign_in(provider, uid, auth_info)
+      user = User.create_from_auth_info(provider, uid, auth_info)
+      sign_in(user)
+      true
     end
   end
 
@@ -53,9 +42,10 @@ class AuthenticationsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     if authentication
-      sign_in_with_authentication(authentication)
+      sign_in authentication.user
+      true
     else
-      create_authentication(provider, uid, auth_hash['info'])
+      create_authentication_and_sign_in(provider, uid, auth_hash['info'])
     end
   end
 end

f7f9840e662c355321480a05f9721c02438a36af
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-11-19 19:25:20 -0200

    Refactoring: swipe under the carpet the decision making for create authentications or not.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -16,26 +16,46 @@ class AuthenticationsController < ApplicationController
   end
 
   private
+  def sign_in_with_authentication(authentication)
+    if user_signed_in?
+      current_user.authentications.create(provider: authentication.provider, uid: uid)
+    else
+      sign_in authentication.user
+    end
+    true
+  end
+
+  def sign_in_with_twitter(uid)
+    if user_signed_in?
+      current_user.authentications.create(provider: 'twitter', uid: uid)
+      true
+    else
+      redirect_to new_user_session_path, notice: I18n.t('auth.cant_create_twitter')
+      false
+    end
+  end
+
+  def create_authentication_and_sign_in(provider, uid, auth_info)
+    user = User.create_from_auth_info(provider, uid, auth_info)
+    sign_in(user)
+    true
+  end
+
+  def create_authentication(provider, uid, auth_info)
+    if provider.to_s == 'twitter'
+      sign_in_with_twitter(uid)
+    else
+      create_authentication_and_sign_in(provider, uid, auth_info)
+    end
+  end
 
   def authenticate!(provider, uid, user_info)
     auth_hash = request.env['omniauth.auth']
     authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-
     if authentication
-      sign_in authentication.user unless user_signed_in?
-      true
-    elsif current_user
-      current_user.authentications.create(:provider => provider, :uid => uid)
-      true
+      sign_in_with_authentication(authentication)
     else
-      if provider.to_s == 'twitter'
-        redirect_to new_user_session_path, notice: I18n.t('auth.cant_create_twitter')
-        false
-      else
-        user = User.create_from_auth_info(provider, uid, auth_hash['info'])
-        sign_in(user)
-        true
-      end
+      create_authentication(provider, uid, auth_hash['info'])
     end
   end
 end

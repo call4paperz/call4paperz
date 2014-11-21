2cb0c8111651faebc93726e95abd2e49a7ec6017
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-11-21 16:45:40 -0200

    Add script to promote twitter avatars to photos

---
+++ spec/models/twitter_avatar_promotion_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe TwitterAvatarPromotion do
+  let(:fulano) { FactoryGirl.create :user }
+  let(:promoter) { TwitterAvatarPromotion.new fulano }
+
+  before(:all) do
+    path = Rails.root + 'spec/fixtures/'
+    @server_pid = spawn "ruby -run -e httpd #{path} -p 3101"
+  end
+
+  after(:all) do
+    Process.kill 'KILL', @server_pid
+  end
+
+  it 'copy a valid twitter avatar' do
+    fulano.twitter_avatar = 'http://localhost:3101/image_1.png'
+    fulano.save!
+    expect(promoter.promote).to eq true
+    expect(fulano.photo.filename).to eq 'image_1.png'
+  end
+
+  it 'do not copy a invalid avatar' do
+    fulano.twitter_avatar = 'http://localhost:3101/nothing-404'
+    fulano.save!
+    expect(promoter.promote).to eq false
+    expect(fulano.photo?).to eq false
+  end
+
+  it 'do not override the current photo with twitter avatar' do
+    fulano.twitter_avatar = 'http://localhost:3101/image_1.png'
+    fulano.remote_photo_url = 'http://localhost:3101/image_2.png'
+    fulano.save!
+    promoter.promote
+    expect(fulano.photo.filename).to eq 'image_1.png'
+  end
+end

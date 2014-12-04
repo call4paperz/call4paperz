b577e4b968e120d27423856db574b4bf7f65ec53
Lucas Mazza <lucastmazza@gmail.com>
2014-12-04 18:33:50 -0200

    Restore `FactoryGirl.stub` call that was converted by mistake.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ApplicationHelper, :type => :helper do
-  let(:user) { allow(FactoryGirl).to receive(:user) }
+  let(:user) { FactoryGirl.stub(:user) }
 
   describe "#menu" do
     subject { helper.menu }

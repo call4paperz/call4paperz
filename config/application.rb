6691ee9b0e3dd4bc0b383d7f9ac6d3fc0d9ba589
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2015-03-17 16:58:41 -0300

    Remove deprecations warnings (rails 5).

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Rr10Team71
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end

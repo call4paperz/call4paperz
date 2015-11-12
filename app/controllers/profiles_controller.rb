041d1fee992a717d5b96cf933e78b4cfeab59f22
Rafael B. Tauil <rafael@tauil.com.br>
2015-11-12 00:31:31 -0200

    Improves how unconfirmed e-mail is shown and adds resend confirmation e-mail link

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  before_filter :load_user, only: [:edit, :update]
+  before_filter :load_user, only: [:edit, :update, :resend_confirmation_email]
 
   def show
     @profile = Profile.new current_user
@@ -10,12 +10,18 @@ class ProfilesController < ApplicationController
 
   def update
     if @user.update_attributes user_params
-      force_send_confirmation_email if @user.email.blank? && user_params[:email].present?
       flash[:notice] = I18n.t('flash.notice.profile_updated')
+      set_mail_changes if changed_email?
       redirect_to profile_path
     else
       render :edit
     end
+  end
+
+  def resend_confirmation_email
+    flash[:notice] = I18n.t('flash.notice.profile_email_confirmation_sent')
+    force_send_confirmation_email
+    redirect_to edit_profile_path(@user)
   end
 
   private
@@ -26,6 +32,15 @@ class ProfilesController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :photo, :photo_cache)
+  end
+
+  def changed_email?
+    user_params[:email].present?
+  end
+
+  def set_mail_changes
+    flash[:notice] = I18n.t('flash.notice.profile_email_updated')
+    force_send_confirmation_email if @user.email.blank?
   end
 
   def force_send_confirmation_email

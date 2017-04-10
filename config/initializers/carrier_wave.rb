a29ad898b529668abf1ac6c3da62fdc99153ddd6
Rafael B. Tauil <rafael@tauil.com.br>
2017-04-10 13:16:06 -0300

    Fixes deploying to heroku
    I've got the error during the deploy: 'uninitialized constant CarrierWave::Storage::Fog'

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,3 +1,7 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog

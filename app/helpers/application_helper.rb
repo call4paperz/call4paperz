f8bd6e3ee62702c451e26a28a295ba30fa4d5fe7
Rafael B. Tauil <rafael@tauil.com.br>
2017-04-13 00:40:47 -0300

    More improvements and changes to event page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
   def menu
-    content_tag :ul, :id  => "menu_top" do
+    content_tag :nav, id: "main-nav" do
       content = ''
-      content << content_tag(:li, link_to("Home", root_url))
-      content << content_tag(:li, link_to("Events", events_path))
+      content << link_to("Home", root_url)
+      content << link_to("Events", events_path)
       if user_signed_in?
-        content << content_tag(:li, link_to("Profile", profile_path))
-        content << content_tag(:li, link_to("Logout", destroy_user_session_path, :method => :delete))
+        content << link_to("Profile", profile_path)
+        content << link_to("Logout", destroy_user_session_path, :method => :delete)
       else
-        content << content_tag(:li, link_to("Login", new_user_session_path))
+        content << link_to("Login", new_user_session_path)
       end
       content.html_safe
     end

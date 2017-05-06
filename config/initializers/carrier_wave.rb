b0ad98d7d895a3e9c230028d4ef1f73b5709af91
Luciano Sousa <ls@lucianosousa.net>
2017-05-06 18:25:55 -0300

    not required config?

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,6 @@ require 'carrierwave/storage/fog'
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage         = :fog,
-    config.fog_provider    = 'fog/aws',
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_ACCESS_KEY'],

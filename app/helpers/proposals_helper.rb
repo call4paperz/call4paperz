5fb7caf215e3cf460073192dd483a0a3914a8954
Rafael B. Tauil <rafael@tauil.com.br>
2017-10-25 22:03:33 -0200

    Removes unused helpers and specs

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -8,44 +8,4 @@ module ProposalsHelper
     end
   end
 
-  def vote_box(event, proposal, user)
-    if user&.has_vote_for?(proposal)
-      image_tag('checked.png', :alt => 'Thanks for voting!').html_safe
-    else
-      content = ''
-      ajax_class = {:class => "ajax_vote"} if user
-      content << link_to(image_tag('positive.png', :class => 'thumb thumb_p'), like_event_proposal_path(event, proposal), ajax_class || {})
-      content << link_to(image_tag('negative.png', :class => 'thumb'), dislike_event_proposal_path(event, proposal), ajax_class || {})
-      content.html_safe
-    end
-  end
-
-  def render_votes_bar(votes_count)
-    if votes_count == 50.0
-      klass = 'gray'
-    elsif votes_count > 0
-      klass = 'green'
-    else
-      klass = 'red'
-      votes_count = votes_count.abs
-    end
-
-    content_tag :div, :class => 'percentage' do
-      content_tag :div, :class => "#{klass}", :style => "width: #{votes_bar_width(votes_count)}px" do
-        content_tag :span, votes_count
-      end
-    end
-  end
-
-  private
-    VOTES_BAR_MAX_WIDTH = 200 # In pixels
-    LIMIT = 100
-
-    def votes_bar_width(value)
-      if value >= LIMIT
-        VOTES_BAR_MAX_WIDTH
-      else
-        value * VOTES_BAR_MAX_WIDTH / LIMIT
-      end
-    end
 end
--- spec/helpers/proposals_helper_spec.rb
+++ spec/helpers/proposals_helper_spec.rb
@@ -1,58 +1,7 @@
 require 'spec_helper'
 
 describe ProposalsHelper, :type => :helper do
-  let(:user) { FactoryGirl.create(:user) }
-  let(:proposal) { FactoryGirl.create(:proposal) }
-  let(:event) { FactoryGirl.create(:event) }
 
-  describe "#vote_box" do
-    context "user is set and has voted" do
-      before(:each) do
-        Vote.like(proposal, user)
-      end
+  describe "#proposal_user"
 
-      subject { helper.vote_box(event, proposal,user) }
-      it { is_expected.to match(/Thanks for voting!/)}
-    end
-
-    context "no user" do
-      subject { helper.vote_box(event, proposal, nil) }
-      it { is_expected.to match(like_event_proposal_path(event, proposal)) }
-      it { is_expected.to match(dislike_event_proposal_path(event, proposal)) }
-      it { is_expected.not_to match("ajax_vote") }
-    end
-  end
-
-  describe "#render_votes_bar" do
-    context "when receives -50 votes" do
-      it "returns progress bar with 100px width" do
-        expect(helper.render_votes_bar(-50)).to include %{<div class="red" style="width: 100px"><span>50</span></div>}
-      end
-    end
-    context "when receives 0 votes" do
-      it "returns progress bar with 0px width" do
-        expect(helper.render_votes_bar(0)).to include %{<div class="red" style="width: 0px"><span>0</span></div>}
-      end
-    end
-    context "when receives 50 votes" do
-      it "returns progress bar with 100px width" do
-        expect(helper.render_votes_bar(50)).to include %{<div class="gray" style="width: 100px"><span>50</span></div>}
-      end
-    end
-    context "when receives 100 votes" do
-      it "returns progress bar with 200px width" do
-        expect(helper.render_votes_bar(100)).to include %{<div class="green" style="width: 200px"><span>100</span></div>}
-      end
-    end
-    context "when receives more than 100 votes" do
-      it "returns progress bar with 200px width" do
-        expect(helper.render_votes_bar(136)).to include %{<div class="green" style="width: 200px"><span>136</span></div>}
-      end
-    end
-    context "when receives more than 100 negatives votes" do
-      it "returns progress bar with 100px width" do
-        expect(helper.render_votes_bar(-140)).to include %{<div class="red" style="width: 200px"><span>140</span></div>}
-      end
-    end
-  end
 end

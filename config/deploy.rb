4a25249728ddea4b7b5d4bcff335b723b53ac1ba
Luciano Sousa <ls@lucianosousa.net>
2018-05-05 15:48:52 -0300

    Cleaning (#243)
    * removing not used files

--- config/deploy.rb
+++
@@ -1,8 +0,0 @@
-application = 'call4paperz'
-repository = 'git@github.com:railsrumble/rr10-team-71.git'
-hosts = %w(call4paperz.com)
-path = "/u/apps/#{application}"
-user = 'call4paperz'
-ssh_opts = '-A'
-branch = 'production'
-login_shell = true

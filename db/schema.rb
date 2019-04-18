c404001305ea78b67383e01a4f89a48a43e94ee3
Frankyston Lins <frankyston@gmail.com>
2019-04-18 19:42:09 -0300

    Update Ruby and Rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317224432) do
+ActiveRecord::Schema.define(version: 2015_03_17_224432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
